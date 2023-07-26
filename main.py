a083cfbd0d83dbe9226148ca37dfd719d6b88527
Alex Bush <alex.pheonixtechnical@gmail.com>
2023-07-26 10:50:30 +0100

    fix: error handling

--- main.py
+++ main.py
@@ -1,84 +1,15 @@
 import http.server
 import multiprocessing as mp
+import socketserver
+import threading
 import time
 import os
 from multiprocessing import freeze_support
 from socketserver import ThreadingMixIn
+from typing import Tuple
 
 import cv2
-
-
-class Camera():
-
-    def __init__(self, rtsp_url):
-        # load pipe for data transmittion to the process
-        self.parent_conn, child_conn = mp.Pipe()
-        # load process
-        self.p = mp.Process(target=self.update, args=(child_conn, rtsp_url))
-        # start process
-        self.p.daemon = True
-        self.p.start()
-
-    def end(self):
-        # send closure request to process
-
-        self.parent_conn.send(2)
-
-    def update(self, conn, rtsp_url):
-        # load cam into seperate process
-
-        print("Cam Loading...")
-        os.putenv("OPENCV_FFMPEG_CAPTURE_OPTIONS", "rtsp_transport;tcp")
-        cap = cv2.VideoCapture(rtsp_url, cv2.CAP_FFMPEG)
-        print("Cam Loaded...")
-        run = True
-
-        while run:
-
-            # grab frames from the buffer
-            cap.grab()
-
-            # recieve input data
-            rec_dat = conn.recv()
-
-            if rec_dat == 1:
-                # if frame requested
-                ret, frame = cap.read()
-                conn.send(frame)
-
-            elif rec_dat == 2:
-                # if close requested
-                cap.release()
-                run = False
-
-        print("Camera Connection Closed")
-        conn.close()
-
-    def get_frame(self, resize=None):
-        ###used to grab frames from the cam connection process
-
-        ##[resize] param : % of size reduction or increase i.e 0.65 for 35% reduction  or 1.5 for a 50% increase
-
-        # send request
-        self.parent_conn.send(1)
-        frame = []
-        try:
-            frame = self.parent_conn.recv()
-        except EOFError as e:
-            print("ERROR!")
-            print(e)
-        # reset request
-        self.parent_conn.send(0)
-
-        # resize if needed
-        if resize == None:
-            return frame
-        else:
-            return self.rescale_frame(frame, resize)
-
-    def rescale_frame(self, frame, percent=65):
-
-        return cv2.resize(frame, None, fx=percent, fy=percent)
+import numpy as np
 
 
 class CamHandler(http.server.BaseHTTPRequestHandler):
@@ -89,7 +20,10 @@ class CamHandler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
             self.end_headers()
             while True:
-                img = cam_mjpeg.get_frame()
+                if self.server.started == True:
+                    img = self.server.frame
+                else:
+                    img = np.zeros((1, 1, 3), dtype=np.uint8)
                 r, buf = cv2.imencode(".jpg", img)
                 self.wfile.write("--jpgboundary\r\n".encode())
                 self.send_header('Content-type', 'image/jpeg')
@@ -102,7 +36,7 @@ class CamHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
             self.end_headers()
-            img = cam_jpeg.get_frame()
+            img = self.server.frame
             r, buf = cv2.imencode(".jpg", img)
             self.wfile.write("--jpgboundary\r\n".encode())
             self.send_header('Content-type', 'image/jpeg')
@@ -126,6 +60,19 @@ class ThreadedHTTPServer(ThreadingMixIn, http.server.HTTPServer):
     """Handle requests in a separate thread."""
 
 
+def thread_function(rtsp_url, server):
+    print("Cam Loading...")
+    os.putenv("OPENCV_FFMPEG_CAPTURE_OPTIONS", "rtsp_transport;tcp")
+    cap = cv2.VideoCapture(rtsp_url, cv2.CAP_FFMPEG)
+    print("Cam Loaded...")
+    while True:
+        server.started = True
+        try:
+            ret, server.frame = cap.read()
+        except:
+            print("EEEE")
+
+
 if __name__ == '__main__':
     freeze_support()
 
@@ -134,16 +81,16 @@ if __name__ == '__main__':
         port = 8000
 
     server = ThreadedHTTPServer(('', port), CamHandler)
+    server.frame = np.zeros((1, 1, 3), dtype=np.uint8)
+    server.started = False
     time.sleep(5)
     rtsp_path = os.getenv("RTSP_URL")
     if not rtsp_path:
         print("RTSP_URL environment varaible not defined")
         exit(-1)
-    cam_mjpeg = Camera(rtsp_path)
-    cam_jpeg = Camera(rtsp_path)
 
-    print(f"MJPEG Camera is alive?: {cam_mjpeg.p.is_alive()}")
-    print(f"JPEG Camera is alive?: {cam_jpeg.p.is_alive()}")
+    mjpeg = threading.Thread(target=thread_function, args=(rtsp_path, server), daemon=True)
+    mjpeg.start()
 
     print("server started on: ")
     server.serve_forever()
