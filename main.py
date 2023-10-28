3db5b0c1153cd14ad9b6f7a45a5a37bf926fd6d7
Alex <alex@tts>
2023-10-28 16:09:00 +0100

    fix: streamer

--- main.py
+++ main.py
@@ -72,6 +72,8 @@ def thread_function(rtsp_url, server):
         try:
             logging.info("Read")
             ret, frame = cap.read()
+            if not frame:
+                frame = np.zeros((1, 1, 3), dtype=np.uint8)
             logging.info(ret)
             r2, frameOut = cv2.imencode(".jpg", frame)
             logging.info(r2)
