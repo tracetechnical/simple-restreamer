240bbdcfe5468ed14bbc8353e928837eadc3656d
Alex <alex@tts>
2023-10-28 16:53:13 +0100

    fix: streamer

--- main.py
+++ main.py
@@ -53,11 +53,10 @@ def open_cam_rtsp(uri, width, height, latency):
     """Open an RTSP URI (IP CAM)."""
     gst_str = (
         'rtspsrc location={} latency={} drop-on-latency=true ! rtph264depay ! h264parse ! avdec_h264 ! videoscale '
-        'method=0 add-borders=false ! video/x-raw,width=2000,height=1000  ! videoflip method=horizontal-flip ! '
+        'method=0 add-borders=false ! video/x-raw,width=1500,height=750  ! videoflip method=vertical-flip ! '
         'videoconvert ! appsink').format(
         uri, latency)
     logging.info("gst:" + gst_str)
-    time.sleep(5)
     return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
 
 
