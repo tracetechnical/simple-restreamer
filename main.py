55be20b7aaa52b1306e8aa23c1bcebb2db59f9e2
Alex <alex@tts>
2024-03-28 14:16:34 +0000

    fix: add buffer, latency 0

--- main.py
+++ main.py
@@ -60,8 +60,8 @@ def open_cam_rtsp(uri, rotation, latency):
 
 
     gst_str = (
-        'rtspsrc location={} latency={} drop-on-latency=true ! rtph264depay ! h264parse ! decodebin ! '
-        'videoconvert ! appsink').format(
+        'rtspsrc location={} latency=0 buffer-mode=auto ! rtph264depay ! h264parse ! decodebin ! '
+        + rotation_str + ' ! videoconvert ! appsink').format(
         uri, latency)
     logging.info("gst:" + gst_str)
     return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
